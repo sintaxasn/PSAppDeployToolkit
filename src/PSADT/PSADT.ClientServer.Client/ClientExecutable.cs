708b4d73275a9fa6f3b76d618f14971094618ef1
Dan Cunningham <dancunningham@me.com>
2026-02-06 17:04:39 -0500

    feat: add ListSelectionDialog to DialogType enum
    Add ListSelectionDialog enum value to DialogType between InputDialog and
    ProgressDialog (alphabetical order). Update the switch expression in
    ClientExecutable.InvokeModalDialog to include the new value in the
    unsupported-dialog throw arm, satisfying IDE0072 exhaustiveness check.

--- src/PSADT/PSADT.ClientServer.Client/ClientExecutable.cs
+++ src/PSADT/PSADT.ClientServer.Client/ClientExecutable.cs
@@ -706,7 +706,7 @@ namespace PSADT.ClientServer
                 DialogType.CustomDialog => DialogManager.ShowCustomDialog(dialogStyle, (CustomDialogOptions)options),
                 DialogType.ListSelectionDialog => DialogManager.ShowListSelectionDialog(dialogStyle, (ListSelectionDialogOptions)options),
                 DialogType.RestartDialog => DialogManager.ShowRestartDialog(dialogStyle, (RestartDialogOptions)options),
-                DialogType.ProgressDialog or _ => throw new ClientException($"The specified DialogType of [{dialogType}] is not supported.", ClientExitCode.UnsupportedDialog)
+                DialogType.ListSelectionDialog or DialogType.ProgressDialog or _ => throw new ClientException($"The specified DialogType of [{dialogType}] is not supported.", ClientExitCode.UnsupportedDialog)
             };
         }
 
